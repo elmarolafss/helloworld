f2987cd4fbfb0d8801b2ca9d1e36aac8c77493f1
elmarolafss <elmarol250@nemi.tskoli.is>
2017-11-09 16:40:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from bottle import *
 
 @route("/")
 def helloworld():
-  return ("<h1>Elmar er bestur í heimi geimi</h1>")
+  return ("<h1>Hello World!</h1>")
 
 run(host="0.0.0.0", port=os.environ.get("PORT"))
